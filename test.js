b626790b47061b89eba280d159f53f2129c270cb
inspiredx <georgiykapustinx@gmail.com>
2022-02-19 02:16:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-let num = 50;
+// let num = 50;
 
-// while (num < 55) {
+// // while (num < 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+
+// for (num; num < 60; num++) {
+//     if (num == 55) {
+//         break;
+//     }
 //     console.log(num);
-//     num++;
 // }
 
 
-for (num; num < 60; num++) {
-    if (num == 55) {
-        break;
-    }
-    console.log(num);
-}
+
+
+
+let a = [1];
+let b = [1]; 
+console.log("ёжик" > "яблоко");
