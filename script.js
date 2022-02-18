c8faf9e8047c972326d6c3948d828b2b36fc8f34
inspiredx <georgiykapustinx@gmail.com>
2022-02-18 09:36:49 +0300

    Lesson 1

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let money = prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет на месяц?');
+let time = +prompt('Введите дату в формате YYYY-MM-DD');
+
+let firstQuestion = prompt('Введите обязательную статью расходов в этом месяце');
+let secondQuestion = prompt('Во сколько обойдется?');
 
 let appData = {
     budget: money,
     timeData: time,
-    // expenses,
-    // optionalExpenses,
+    expenses: {
+        firstQuestion: secondQuestion
+    },
+    optionalExpenses: {},
     income: [],
     savings: false
 };
+
+let dayBudget = Math.round(appData.budget / 30);
+
+
+document.getElementById("min").innerHTML = dayBudget;
