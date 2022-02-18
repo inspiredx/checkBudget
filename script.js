7ef34ae8f6e6324fdfe4808257eb8d02b361238c
inspiredx <georgiykapustinx@gmail.com>
2022-02-18 08:34:54 +0300

    1

---
+++ script.js
@@ -0,0 +1,11 @@
+let money = prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    // expenses,
+    // optionalExpenses,
+    income: [],
+    savings: false
+};
