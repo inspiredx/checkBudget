4c12be431384901a67d40818fb4c3c0b4e54385f
inspiredx <georgiykapustinx@gmail.com>
2022-02-19 16:51:43 +0300

    Day 2

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-let money = +prompt('Ваш бюджет на месяц?');
-let time = +prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет на месяц?'); //Благодаря унарному плюсу здесь передается число.
+let time = prompt('Введите дату в формате YYYY-MM-DD');
 
-let firstQuestion = prompt('Введите обязательную статью расходов в этом месяце');
-let secondQuestion = prompt('Во сколько обойдется?');
+
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        firstQuestion: secondQuestion
-    },
+    expenses: {}, //Это объект. Чтобы создать новое свойство или метод у объекта 2 способа: через точку или [].
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
-let dayBudget = Math.round(appData.budget / 30);
+
+//Записываем цикл, который задаст вопросы 2 раза
+
+for (let i = 0; i < 2; /*пока не закончатся вопросы, всего два комплекса вопросов*/ i++) {
+    /*Записываем действия, которые должны произойти*/
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt("Во сколько обойдется?", '');
 
 
-document.getElementById("min").innerHTML = dayBudget;
+    //Пишем условие для цикла
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+        a != '' && b != '' && a.length < 20) { //Проверяем что тип данных - строка. Для prompt не имеет смысла./ Проверяем что НЕ равно null. И то же для b. Не может быть пустой строкой
+        console.log('done');
+        appData.expenses[a] = b;
+        /*При использовании квадратных скобок, свойство может содержать любую строку. 
+           При ответе на вопрос a - ответ будет записан как свойство объекта appData.expenses. Получается пара (ключ-значение) При вводе данные записываются*/
+    } else { //Возвращение на цикл обратно 
+
+    }
+
+
+
+}
+
+
+
+appData.moneyPerDay = Math.round(appData.budget / 30);
+
+alert(appData.moneyPerDay);
+
+//Цикл проверки уровня бюджета
+if (appData.moneyPerDay < 1000) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 5000) { //В противном случае
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 5000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
--- test.js
+++ test.js
@@ -1,22 +1,33 @@
-// let num = 50;
+let num = 50;
 
-// // while (num < 55) {
-// //     console.log(num);
-// //     num++;
-// // }
-
-
-// for (num; num < 60; num++) {
-//     if (num == 55) {
-//         break;
-//     }
-//     console.log(num);
-// }
+while (num < 55) {
+    console.log(num);
+    num++;
+}
 
 
+for (num; num < 60; num++) {
+    if (num == 55) {
+        break;
+    }
+    console.log(num);
+}
+
+do {
+    console.log(num);
+    num++;
+
+} while (num < 55);
 
 
 
-let a = [1];
-let b = [1]; 
-console.log("ёжик" > "яблоко");
+// Аналог цикла for из 3 задания
+
+let i = 0;
+
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt("Во сколько обойдется?", '');
+    i++;
+
+} while (i > 0 && i < 2); //цикл выполняется пока i не перестанет быть меньше 2
